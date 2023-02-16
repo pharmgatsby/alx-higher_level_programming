201ac72a768e96853ea01ffbd9172710cf00fa8b
pharmgatsby <pharmgatsby981@gmail.com>
2023-02-16 14:03:16 -0800

    help my life

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,8 @@
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- uses a database to list all rows not linked to one row
+SELECT name
+FROM tv_genres
+WHERE name NOT IN
+(SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
